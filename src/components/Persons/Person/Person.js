28ca16292e3ceb8c1a358aecac254d2191d7d130
Codilot <e.kooijman@gmail.com>
2018-09-26 15:58:28 +0200

    Ad with classes to person component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react';
 
 import classes from './Person.css';
+import WithClass from '../../../hoc/withClass'
 
 class Person extends PureComponent {
 
@@ -39,13 +40,13 @@ class Person extends PureComponent {
 
     render() {
         return (
-            <div className={classes.Person}>
+            <WithClass classes={classes.Person}>
                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name} />
-            </div>
+            </WithClass>
         );
-        
+
         // return (
         //     [
         //         <p key="1" onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>,
