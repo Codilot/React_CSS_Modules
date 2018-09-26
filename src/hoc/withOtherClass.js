7dfdbd0e2dbe432bcbabe902c1774b9a1172f328
Codilot <e.kooijman@gmail.com>
2018-09-26 16:38:16 +0200

    hoc as stateful component

--- src/hoc/withOtherClass.js
+++ src/hoc/withOtherClass.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
+
+// const withOtherClass = (WrappedComponent, className) => {
+//     return (props) => {
+//         return (
+//         <div className={className}>
+//             <WrappedComponent {...props} />
+//         </div>
+//         );
+//     };
+// }
 
 const withOtherClass = (WrappedComponent, className) => {
-    return (props) => {
-        return (
-        <div className={className}>
-            <WrappedComponent {...props} />
-        </div>
-        );
+    return class extends Component {
+        render () {
+            return (
+                <div className={className}>
+                    <WrappedComponent {...this.props} />
+                </div>
+                );
+
+            }
     };
 }
 
