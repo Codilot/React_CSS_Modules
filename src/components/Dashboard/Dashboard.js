906ca120b8c362b4120ec36a6e2db6edf48679a8
Codilot <e.kooijman@gmail.com>
2018-09-26 15:24:44 +0200

    Classes as arrays, jsx arrays, manual keys, fragment

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,24 +1,25 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import classes from './Dashboard.css';
 
+
 const dashboard = (props) => {
-    
+
     const assignedClasses = [];
-    let btnClass = '';
+    let btnClass = classes.Button;
 
     if (props.showPersons) {
-        btnClass = classes.Red;
+        btnClass = [classes.Button, classes.Red].join(' ');
     }
-  
+
     if (props.persons.length <= 2) {
-      assignedClasses.push(classes.red); // classes = ['red']
+        assignedClasses.push(classes.red); // classes = ['red']
     }
     if (props.persons.length <= 1) {
-      assignedClasses.push(classes.bold); // classes = ['red', 'bold']
+        assignedClasses.push(classes.bold); // classes = ['red', 'bold']
     }
 
     return (
-        <div className={classes.Dashboard}>
+        <Fragment>
             <h1>{props.appTitle}</h1>
             <p className={assignedClasses.join(' ')}>My persons dashboard</p>
             <button
@@ -26,8 +27,7 @@ const dashboard = (props) => {
                 onClick={props.toggle}>
                 Toggle Persons
             </button>
- 
-        </div>
+        </Fragment>
     );
 }
 
